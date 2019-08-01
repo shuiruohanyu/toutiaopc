cc702934cf4c1d043c7a1c6b5282ec4e579ad00f
gaoly <shuiruohanyu@foxmail.com>
2019-08-01 18:20:02 +0800

    提交校验数据

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,19 +2,19 @@
   <div class='login'>
     <el-card class="box-card">
       <img src="../../assets/logo_index.png" alt="">
-      <el-form ref="form">
-        <el-form-item>
-          <el-input placeholder="请输入手机号"></el-input>
+      <el-form ref="form" :model="formData" :rules="rules" :status-icon="true">
+        <el-form-item prop="mobile">
+          <el-input v-model="formData.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="formData.code" placeholder="请输入验证码" style='width:240px'></el-input>
+          <el-button  style="float:right">发送验证码</el-button>
         </el-form-item>
         <el-form-item>
-          <el-input placeholder="请输入验证码" style='width:240px'></el-input>
-          <el-button style="float:right">发送验证码</el-button>
+          <el-checkbox :checked="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-checkbox checked="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
-        </el-form-item>
-        <el-form-item>
-          <el-button type='primary' style='width:100%'>登录</el-button>
+          <el-button @click="login" type='primary' style='width:100%'>登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -22,12 +22,58 @@
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    login () {
+      // 登录方法
+      // 要进行手机号和验证码的校验
+      this.$refs.form.validate((isOk) => {
+        if (isOk) {
+          // 如果一切校验通过 需要调用登录接口
+        }
+      })
+    }
+  },
+  data () {
+    // var phoneNum = (rule, value, callback) => {
+    //   if (value === '') {
+    //     callback(new Error('请输入手机号'))
+    //   } else {
+    //     callback()
+    //   }
+    // }
+    return {
+      // 定义数据
+      formData: {
+        mobile: '',
+        code: ''
+      },
+      //  定义数据规则 给elementUI的表单使用
+      rules: {
+        mobile: [{
+          required: true,
+          message: '手机号不能为空',
+          trigger: 'blur' },
+        {
+          len: 11, message: '手机长度必须为11个字符', trigger: 'blur'
+        }],
+        code: [{
+          required: true,
+          message: '验证码不能为空'
+        },
+        {
+          len: 6, message: '验证码长度必须为6个字符', trigger: 'blur'
+        }
+        ]
+      }
+    }
+  }
+}
 </script>
 
 <style lang='less' scoped>
 .login {
-  background-image: url("../../assets/login_bg.jpg");
+  background-image: url('../../assets/login_bg.jpg');
   width: 100%;
   height: 100vh;
   background-size: cover;
