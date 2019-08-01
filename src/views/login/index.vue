6e8bca95d059775dc90d06793a17f7c46e99ac0f
gaoly <shuiruohanyu@foxmail.com>
2019-08-01 15:47:21 +0800

    完成(elementUI)登录页布局及编码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,18 +1,47 @@
 <template>
-    <div class='login'>Login</div>
+  <div class='login'>
+    <el-card class="box-card">
+      <img src="../../assets/logo_index.png" alt="">
+      <el-form ref="form">
+        <el-form-item>
+          <el-input placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="请输入验证码" style='width:240px'></el-input>
+          <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox checked="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type='primary' style='width:100%'>登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style lang='less' scoped>
-   .login {
-       background-image: url('../../assets/login_bg.jpg');
-       width: 100%;
-       height: 100vh;
-       background-size: cover;
-   }
+.login {
+  background-image: url("../../assets/login_bg.jpg");
+  width: 100%;
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .box-card {
+    width: 400px;
+    height: 330px;
+    img {
+      display: block;
+      width: 200px;
+      margin: 10px auto;
+    }
+  }
+}
 </style>
