85522e8e8ec0f7c520a3c17bce8dc8d43969f4c0
gaoly <shuiruohanyu@foxmail.com>
2019-08-01 10:42:58 +0800

    按照elementUI

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,14 @@
     <router-view/>
   </div>
 </template>
-
+<script>
+export default {
+  data () {}
+}
+</script>
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
