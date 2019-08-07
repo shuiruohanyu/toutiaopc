f9d790fb924e395a0761516ab4a605eee08373c7
gaoly <shuiruohanyu@foxmail.com>
2019-08-07 10:18:52 +0800

    进度条效果完善

--- src/permission.js
+++ src/permission.js
@@ -1,13 +1,15 @@
 // 权限文件
 import router from './router'
 import nProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 // 三个参数
 router.beforeEach((to, from, next) => {
-  nProgress.start() // 进度条开始
   if (to.path.startsWith('/home') && !checkIsLogin()) {
     // 如果是以/home为开头 说明必须要token认证
     next('/login')
   } else {
+    nProgress.start() // 进度条开始
     next()
   }
 })
