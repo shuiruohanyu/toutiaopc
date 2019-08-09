543407e82676c89f9457f509f549249f2a8cc372
gaoly <shuiruohanyu@foxmail.com>
2019-08-09 10:03:30 +0800

    折叠监听

--- src/components/layout/layout-slider.vue
+++ src/components/layout/layout-slider.vue
@@ -9,14 +9,20 @@
       background-color="#353b4e"
       text-color="#adafb5"
       active-text-color="#ffd04b"
+      :collapse="collapse"
     >
-      <el-menu-item index="/">首页</el-menu-item>
-      <el-submenu index="/home">
+      <el-menu-item index="/">
+        <i class='el-icon-s-home'></i>
+      首页</el-menu-item>
+      <el-submenu index="/home" >
         <template slot="title">
+          <i class='el-icon-s-order'></i>
           <span>内容管理</span>
         </template>
         <template>
-          <el-menu-item index="/home/publish">发布文章</el-menu-item>
+          <el-menu-item index="/home/publish" >
+          发布文章
+          </el-menu-item>
           <el-menu-item index="/home/articles">内容列表</el-menu-item>
           <el-menu-item index="/home/comment">评论列表</el-menu-item>
           <el-menu-item index="/home/material">素材列表</el-menu-item>
@@ -24,6 +30,7 @@
       </el-submenu>
       <el-submenu index="/home/twlist">
         <template slot="title">
+          <i class='el-icon-school'></i>
           <span>粉丝管理</span>
         </template>
         <template>
@@ -33,13 +40,27 @@
           <el-menu-item index="/home/fanslist">粉丝列表</el-menu-item>
         </template>
       </el-submenu>
-      <el-menu-item index="/home/account">账户信息</el-menu-item>
+      <el-menu-item index="/home/account">
+      <i class='el-icon-s-custom'></i>
+      账户信息</el-menu-item>
     </el-menu>
   </div>
 </template>
 
 <script>
-export default {}
+import event from '../../utils/event'
+export default {
+  created () {
+    event.$on('collapseChange', () => {
+      this.collapse = !this.collapse
+    })
+  },
+  data () {
+    return {
+      collapse: false
+    }
+  }
+}
 </script>
 
 <style lang='less' scoped>
